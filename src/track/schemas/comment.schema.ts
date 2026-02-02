98fa7b72d58315c9fa1f03bf1479c6abd057c130
00DemiX00 <alescha.yarkin@yandex.ru>
2026-02-02 23:49:16 +0300

    feat: track.service.ts processing

--- src/track/schemas/comment.schema.ts
+++ src/track/schemas/comment.schema.ts
@@ -1,11 +1,12 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 import * as mongoose from 'mongoose'
+import { Track } from "./track.schema";
 
 export type CommentDocument = Comment & Document;
 
 @Schema()
-export class Track {
+export class Comment {
     @Prop()
     username: string;
 
